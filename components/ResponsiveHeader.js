e34bd40240f07eba9225c7ff12baa1973ba5fa0e
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-01-03 17:18:46 +0530

    dgf

--- components/ResponsiveHeader.js
+++ components/ResponsiveHeader.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { NavbarLinks } from "./Header";
 
 const ResponsiveHeader = ({ showMenu, setShowMenu }) => {
-  console.log("showMenu", showMenu);
+  // console.log("showMenu", showMenu);
   return (
     <div
       className={`${
