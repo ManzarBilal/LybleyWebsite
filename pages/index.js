dcfeae98f0886b5c00c7effa8cc7e53db6d61744
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-01-04 12:25:26 +0530

    gdghgh

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,31 @@
 
+import HomePage from '@/components/HomePage'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
- 
- 
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>LYBLEY</title>
-      <meta name="description" content="Explore LYBLEY for cutting-edge website and app development, robust e-commerce solutions, and impactful digital marketing. Our expertise extends to comprehensive CRM services and strategic social media marketing. Elevate your brand's digital presence with tailored solutions designed for growth and engagement. Partner with us to unlock the full potential of the digital landscape and drive your business forward" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap" rel="stylesheet"></link>
-    </Head>
-    <main 
-      className="flex flex-col min-h-screen mt-18"
+      <Head>
+        <title>LYBLEY</title>
+        <meta name="description" content="Explore LYBLEY for cutting-edge website and app development, robust e-commerce solutions, and impactful digital marketing. Our expertise extends to comprehensive CRM services and strategic social media marketing. Elevate your brand's digital presence with tailored solutions designed for growth and engagement. Partner with us to unlock the full potential of the digital landscape and drive your business forward" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap" rel="stylesheet"></link>
+      </Head>
+      <main
+        className="flex flex-col   " style={{ marginTop: "70px" }}
       >
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-      <div>dfghj</div>
-     
-    </main>
+        <div >
+         <HomePage />
+        </div>
+
+      </main>
     </>
   )
 }
