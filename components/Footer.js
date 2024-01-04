dec63f0a5d420dd9e67a9365d1a3b108159acb7d
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-01-04 11:25:20 +0530

    update

--- components/Footer.js
+++ components/Footer.js
@@ -129,7 +129,7 @@ const Footer = () => {
             </div>
           </div>
           <div className="flex justify-between items-center  border-t-2 border-gray-300/50 ">
-            <div className="text-center py-5 text-primary  text-white">
+            <div className="text-center py-5 text-primary  ">
               @copyright 2024 All rights reserved || Made  by Lybley India Pvt Ltd.
             </div>
             <div className=" ">
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import '@/styles/globals.css'
+import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 900,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (<>
     <Header />
     <Component {...pageProps} />
